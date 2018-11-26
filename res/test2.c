f8d387aafa11b9b6138891fade5a6e8707f49710
bruthaearl <42983301+bruthaearl@users.noreply.github.com>
2018-11-26 14:03:45 -0600

    Update test2.c

--- res/test2.c
+++ res/test2.c
@@ -10,7 +10,7 @@ R,G,B: PWM values
 reg: starting register for auto-increment
 num: LED num
 */
-typedef struct LED_t{
+typedef struct LED_t {
     uint8_t R;
     uint8_t G;
     uint8_t B;
@@ -26,7 +26,6 @@ void write_led(LED *led);
 
 /*
 Initialize LEDs
-{R, G, B, register, LED number}
 */
 LED LED1 = {0, 0, 0, 0x02, 1};
 LED LED2 = {0, 0, 0, 0x05, 2};
@@ -51,7 +50,7 @@ int main(){
 /*
 Write PWM values to controller
 */
-void write_led(LED *led){
+void write_led(LED *led) {
     
     volatile int r;
     
@@ -71,5 +70,84 @@ void write_led(LED *led){
     printf("R: %X \n", led->R);
     printf("G: %X \n", led->G);
     printf("B: %X \n", led->B);
+    
+}
+
+
+void Initialize_Driver(){
+    
+    // startup delay
+    
+    
+    //Turn the damn thing on
+    //Send start condition
+    I2C_Startup();
+    //Slave Address
+    I2C_Transmit(LED_DRIVER_ADDRESS);
+    //Select MODE1 register 0x00
+    I2C_Transmit(0x00);
+    //Enable main oscillator
+    I2C_Transmit(0x00);
+    //Done with this block
+    I2C_Stop();
+    
+    //Enable output drivers
+    //Send start condition
+    I2C_Startup();
+    //Slave address
+    I2C_Transmit(LED_DRIVER_ADDRESS);
+    //Select LEDOUT0 register 0x14 WITH AUTO INCREMENT
+    I2C_Transmit(0b10000000 | 0x14);
+    //Enable LEDOUT0
+    I2C_Transmit(0xAA);
+    //Enable LEDOUT1
+    I2C_Transmit(0xAA);
+    //Enable LEDOUT2
+    I2C_Transmit(0xAA);
+    //Enable LEDOUT3
+    I2C_Transmit(0xAA);
+    //Done with this block
+    I2C_Stop();
+    
+    //Turn on star LED
+    //Send start condition
+    I2C_Startup();
+    //Slave address
+    I2C_Transmit(LED_DRIVER_ADDRESS);
+    //Select PWM15 register 0x11
+    I2C_Transmit(0x11);
+    //Light em up bb
+    I2C_Transmit(0xFF);
+    //Done with this block
+    I2C_Stop;
+    
+}
+
+void Write_LED(LED *led){
+    
+    // SEED CODE GOES HERE
+    
+    // Generate random number
+    volatile r = rand();
+    
+    //Bit-shift and mask for RGB values
+    led->R = (uint8_t) (r>>16) & 0xFF;
+    led->G = (uint8_t) (r>>8) & 0xFF;
+    led->B = (uint8_t) r & 0xFF;
+    
+    //Send start condition
+    I2C_Startup();
+    //Slave address
+    I2C_Transmit(LED_DRIVER_ADDRESS);
+    //Select PWM0 register 0x02 WITH AUTO INCREMENT
+    I2C_Transmit(0b10100000 | led->reg);
+    //Send R value
+    I2C_Transmit(led->R);
+    //Send G value
+    I2C_Transmit(led->G);
+    //Send B value
+    I2C_Transmit(led->B);
+    //Hopefully things worked?
+    I2C_Stop();
     
 }
